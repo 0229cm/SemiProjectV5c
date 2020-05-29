f81deb173bf0a94f2a6e118ca10c3bfbc15132dd
0229cm <0229cm@gmail.com>
2020-05-29 15:05:48 +0900

    다운로드 기능 추가

--- src/main/java/min/spring/mvc/controller/PdsController.java
+++ src/main/java/min/spring/mvc/controller/PdsController.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -97,4 +99,20 @@ public class PdsController {
         return "redirect:/pds/list";
     }
 
+    // 첨부파일 다운로드하기
+    // 컨트롤러 메서드에 ResponseBody 에노테이션을 사용하면
+    // view 를 이용해서 데이터를 출력하지 않고
+    // HTTP 응답으로 직접 데이터를 전송하겠다는 의미
+    @ResponseBody
+    @RequestMapping(value = "/pds/pdown")
+    public void pdown(HttpServletRequest req,
+                      HttpServletResponse res) {
+
+        FileUpDownUtil util = new FileUpDownUtil();
+        try {
+            util.procDownload(req, res);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
