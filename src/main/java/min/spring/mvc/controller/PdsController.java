a49a5dbf082192804ca32c40b12c866328aeafda
0229cm <0229cm@gmail.com>
2020-05-29 12:06:03 +0900

    파일업로드 기능 보완2

---
+++ src/main/java/min/spring/mvc/controller/PdsController.java
@@ -0,0 +1,100 @@
+package min.spring.mvc.controller;
+
+import min.spring.mvc.service.FileUpDownUtil;
+import min.spring.mvc.service.PdsService;
+import min.spring.mvc.vo.PdsVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Map;
+
+@Controller
+public class PdsController {
+
+    private PdsService psrv;
+
+    @Autowired
+    public PdsController(PdsService psrv) {this.psrv = psrv;}
+
+    // 목록보기
+    @RequestMapping(value = "/pds/list")
+    public ModelAndView list() {
+
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName("layout/layout"); // 뷰이름 지정
+        mv.addObject("action", "../pds/list.jsp");
+
+        // 목록 불러오기
+        ArrayList<PdsVO> plist = psrv.showPds();
+        mv.addObject("plist", plist);
+
+        return mv;
+    }
+
+    // 새글쓰기
+    @RequestMapping(value = "/pds/write")
+    public ModelAndView write() {
+
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("layout/layout"); // 뷰이름 지정
+
+        mv.addObject("action", "../pds/write.jsp");
+
+        return mv;
+    }
+
+    // 새글쓰기
+    @RequestMapping(value = "/pds/write", method = RequestMethod.POST)
+    public String writeok(PdsVO p, HttpServletRequest req) {
+
+        // 업로드 처리
+        FileUpDownUtil util = new FileUpDownUtil();
+        Map<String, String> frmdata = util.procUpload(req);
+
+        // 서비스 객체로 넘김
+        psrv.newPds(p, frmdata);
+
+        return "redirect:/pds/list";
+    }
+
+    // 본문보기
+    @RequestMapping(value = "/pds/view")
+    public ModelAndView view(String pno) {
+
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName("layout/layout"); // 뷰이름 지정
+        mv.addObject("action", "../pds/view.jsp");
+
+        PdsVO p = psrv.showOnePds(pno);
+        mv.addObject("p", p);
+
+        return mv;
+    }
+
+    // 수정하기
+    @RequestMapping(value = "/pds/update")
+    public ModelAndView update() {
+
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("layout/layout"); // 뷰이름 지정
+
+        mv.addObject("action", "../pds/modify.jsp");
+
+        return mv;
+    }
+
+    // 삭제하기
+    @RequestMapping(value = "/pds/delete")
+    public String delete() {
+
+        return "redirect:/pds/list";
+    }
+
+}
---
+++ src/main/java/min/spring/mvc/service/PdsService.java
@@ -0,0 +1,62 @@
+package min.spring.mvc.service;
+
+import min.spring.mvc.dao.PdsDAO;
+import min.spring.mvc.vo.PdsVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+@Service("psrv")
+public class PdsService {
+
+    private PdsDAO pdao;
+
+    @Autowired
+    public PdsService(PdsDAO pdao) {
+        this.pdao = pdao;
+    }
+
+    public String newPds(PdsVO p,
+                         Map<String, String> frmdata) {
+        String result = "데이터 입력 실패!";
+
+        procFormdata(p, frmdata);
+        p.setFdown("0");
+
+        if (pdao.insertPds(p))
+            result = "데이터 입력 성공!!";
+
+        System.out.println(result);
+        // result 변수 값을 WAS 콘솔에 로그형태로 출력
+
+        return result;
+    }
+
+    public ArrayList<PdsVO> showPds() {
+        return (ArrayList<PdsVO>)pdao.selectPds();
+    }
+
+    public PdsVO showOnePds(String pno) {
+        return pdao.selectOnePds(pno);
+    }
+
+    // multipart 폼 데이터 처리
+    private void procFormdata(PdsVO p, Map<String, String > frmdata) {
+
+        for(String key:frmdata.keySet()) {
+            String val = frmdata.get(key);
+            switch (key) {
+                case "title": p.setTitle(val); break;
+                case "userid": p.setUserid(val); break;
+                case "contents": p.setContents(val); break;
+
+                case "file1": p.setFname(val); break;
+                case "file1size": p.setFsize(val); break;
+                case "file1type": p.setFtype(val); break;
+            }
+        }
+    }
+
+}
